31a35eff6eca2a17ca496ce5e4eed83f99b70b82
HectorUVG <87456605+HectorUVG@users.noreply.github.com>
2021-08-21 18:54:28 -0600

    solo falta hacer vista y controlador antes de revisar

--- Familia.java
+++ Familia.java
@@ -9,12 +9,27 @@ public class Familia {
     //*************************************************************************
     //Declaracion de variables
     //*************************************************************************
-    private String perroFam[];
+    /*aqui se guardan los perros y sus caracteristicas. La idea es que en el 
+    primer [] se encuentra el eje y y en el segundo el eje x. cada fila sera
+    un perro con cada una de sus caracteristicas, y la siguiente fila 
+    representa otro perro.
+    */
+    private String perroFam[][];
     //*************************************************************************
     //Constructores
     //*************************************************************************
-
+    public Familia(){
+        this.perroFam = perroFam;
+    }
     //*************************************************************************
     //Metodos
     //*************************************************************************
+    
+    /*Este getter me permite retornar cualquier dato agragado en la matriz de 2 
+    dimensiones perroFam
+    */
+    public String getPerroFam(int i, int j){
+        return perroFam[i][j];
+    }
+
 }
--- Perrera.java
+++ Perrera.java
@@ -9,13 +9,31 @@ public class Perrera {
     //*************************************************************************
     //Declaracion de variables
     //*************************************************************************
-
+    
+    //aqui se guardan las familias que han adoptado
+    private String fam[];
+    //aqui se guardan los perros que no fueron adoptados
+    private String perroPer[][];
     //*************************************************************************
     //Constructores
     //*************************************************************************
-
+    public Perrera(){
+        this.fam = fam;
+        this.perroPer = perroPer;
+    }
     //*************************************************************************
     //Metodos
     //*************************************************************************
     
+    /*
+    retornan datos guardados en los arreglos fam y perroPer, respectivamente
+    */
+    public String getFam(int i){
+        return fam[i];
+    }
+
+    public String getPerroPer(int i, int j){
+        return perroPer[i][j];
+    }
+
 }
