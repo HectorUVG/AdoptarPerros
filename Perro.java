be0c6d2754023ae59dd2d75395a35025e60f2aec
HectorUVG <87456605+HectorUVG@users.noreply.github.com>
2021-08-21 18:06:13 -0600

    Initial commit

---
+++ Perro.java
@@ -0,0 +1,57 @@
+//*****************************************************************************
+//Imports
+//*****************************************************************************
+
+//*****************************************************************************
+//Clase
+//*****************************************************************************
+public class Perro {
+    //*************************************************************************
+    //Declaracion de variables
+    //*************************************************************************
+    private String nombrePerro;
+    private String raza;
+    private String edad;
+    private String tamanio;
+    private String color;
+    //*************************************************************************
+    //Constructores
+    //*************************************************************************
+    public Perro(String nombrePerro, String raza, String edad){
+        this.nombrePerro = nombrePerro;
+        this.raza = raza;
+        this.edad = edad;
+        this.tamanio = tamanio;
+        this.color = color;
+    }
+    
+    //*************************************************************************
+    //Metodos
+    //*************************************************************************
+    
+    //#####################No estoy seguro si el colocar los nombres de los
+    // returns en los getters me va a causar algun error, ya que puede que 
+    // devuelva this.raza en lugar del String asignado#####################
+    public String getnombrePerro(){
+        return nombrePerro;
+    }
+
+    public String getRaza(){
+        return raza;
+    }
+
+    public String getEdad(){
+        return edad;
+    }
+
+    public String getTamanio(){
+        return tamanio;
+    }
+
+    public String getColor(){
+        return color;
+    }
+    
+    
+}
+
